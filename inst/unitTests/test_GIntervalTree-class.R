e63a5d3246ba56136a8acf4da631d2c240c673a4
hcorrada <hcorrada@gmail.com>
2013-05-21 14:30:20 -0400

    non-circular findOverlaps using GIntervalTree implemented

--- inst/unitTests/test_GIntervalTree-class.R
+++ inst/unitTests/test_GIntervalTree-class.R
@@ -82,68 +82,46 @@ test_GIntervalTree_subsetting <- function () {
   
 }
 
-# test_that("findOverlaps works", {
-#   gr <-
-#     GRanges(seqnames =
-#               Rle(c("chr1", "chr2", "chr1", "chr3"), c(1, 3, 2, 4)),
-#             ranges =
-#               IRanges(1:10, width = 10:1, names = head(letters,10)),
-#             strand =
-#               Rle(strand(c("-", "+", "*", "+", "-")),
-#                   c(1, 2, 2, 3, 2)),
-#             score = 1:10,
-#             GC = seq(1, 0, length=10))
-#   gr1 <-
-#     GRanges(seqnames = "chr2", ranges = IRanges(4:3, 6),
-#             strand = "+", score = 5:4, GC = 0.45)
+test_GIntervalTree_findOverlaps <- function() {
+  subject <-
+    GRanges(seqnames =
+              Rle(c("chr1", "chr2", "chr1", "chr3"), c(1, 3, 2, 4)),
+            ranges =
+              IRanges(1:10, width = 10:1, names = head(letters,10)),
+            strand =
+              Rle(strand(c("-", "+", "*", "+", "-")),
+                  c(1, 2, 2, 3, 2)),
+            score = 1:10,
+            GC = seq(1, 0, length=10))
+  query <-
+    GRanges(seqnames = "chr2", ranges = IRanges(4:3, 6),
+            strand = "+", score = 5:4, GC = 0.45)
   
-#   git <- GIntervalTree(gr)
+  stree <- GIntervalTree(subject)
   
-#   olaps2=findOverlaps(gr1, gr)
-#   olaps1=findOverlaps(gr1, git)
-#   expect_equal(olaps1,olaps2)
-# })
+  olaps2=findOverlaps(query, subject)
+  olaps1=findOverlaps(query, stree)
+  checkIdentical(olaps1, olaps2)
+}
 
-# test_that("subsetByOverlaps,GenomicRanges,GIntervalTree-method works", {
-#   gr <-
-#     GRanges(seqnames =
-#               Rle(c("chr1", "chr2", "chr1", "chr3"), c(1, 3, 2, 4)),
-#             ranges =
-#               IRanges(1:10, width = 10:1, names = head(letters,10)),
-#             strand =
-#               Rle(strand(c("-", "+", "*", "+", "-")),
-#                   c(1, 2, 2, 3, 2)),
-#             score = 1:10,
-#             GC = seq(1, 0, length=10))
-#   gr1 <-
-#     GRanges(seqnames = "chr2", ranges = IRanges(4:3, 6),
-#             strand = "+", score = 5:4, GC = 0.45)
-#   
-#   git=as(gr,"GIntervalTree")
-#   
-#   olaps2=subsetByOverlaps(gr1, gr)
-#   olaps1=subsetByOverlaps(gr1, git)
-#   expect_equal(olaps1,olaps2)
-# })
-# 
-# test_that("subsetByOverlaps,GIntervalTree,GenomicRanges-method works", {
-#   gr <-
-#     GRanges(seqnames =
-#               Rle(c("chr1", "chr2", "chr1", "chr3"), c(1, 3, 2, 4)),
-#             ranges =
-#               IRanges(1:10, width = 10:1, names = head(letters,10)),
-#             strand =
-#               Rle(strand(c("-", "+", "*", "+", "-")),
-#                   c(1, 2, 2, 3, 2)),
-#             score = 1:10,
-#             GC = seq(1, 0, length=10))
-#   gr1 <-
-#     GRanges(seqnames = "chr2", ranges = IRanges(4:3, 6),
-#             strand = "+", score = 5:4, GC = 0.45)
-#   
-#   git=as(gr,"GIntervalTree")
-#   
-#   olaps2=subsetByOverlaps(gr, gr1)
-#   olaps1=subsetByOverlaps(git, gr1)
-#   expect_equal(olaps1,olaps2)
-# })
+test_GIntervalTree_subsetByOverlaps <- function() {
+  subject <-
+    GRanges(seqnames =
+              Rle(c("chr1", "chr2", "chr1", "chr3"), c(1, 3, 2, 4)),
+            ranges =
+              IRanges(1:10, width = 10:1, names = head(letters,10)),
+            strand =
+              Rle(strand(c("-", "+", "*", "+", "-")),
+                  c(1, 2, 2, 3, 2)),
+            score = 1:10,
+            GC = seq(1, 0, length=10))
+  query <-
+    GRanges(seqnames = "chr2", ranges = IRanges(4:3, 6),
+            strand = "+", score = 5:4, GC = 0.45)
+  
+  stree <- as(subject,"GIntervalTree")
+  
+  olaps2 <- subsetByOverlaps(query, subject)
+  olaps1 <- subsetByOverlaps(query, stree)
+  checkIdentical(olaps1,olaps2)
+}
